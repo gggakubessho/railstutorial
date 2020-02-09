197c6ae96da309bbfbe825e1c99ba6c31ca42fa1
GakuBessho <ec2-user@ip-172-31-41-224.ap-northeast-1.compute.internal>
2020-02-09 13:42:29 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      #default で @userはidを渡す
+      #redirect_toはdefaultでuser_path(@user)を渡す
+      redirect_to @user
+      #=>GET user_path(@user)  => show
+    else
+      render 'new'
+    end
+  end
+  
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
