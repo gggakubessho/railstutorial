5cce11e8db4a08a6faf4b204c89e707abfb5efc1
GakuBessho <ec2-user@ip-172-31-41-224.ap-northeast-1.compute.internal>
2020-02-11 07:27:23 +0000

    Finish ch11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      #default で @userはidを渡す
-      #redirect_toはdefaultでuser_path(@user)を渡す
-      redirect_to @user
-      #=>GET user_path(@user)  => show
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,12 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  host = '86db607e9a6c4326ae02434a5faf7799.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  #host = 'gb-sample-app.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
